2f37365f4a981776f5e5d066454ed4669dc335ed
ANDREW WEBSTER 14023216 <14023216@uhi.ac.uk>
2023-05-17 15:39:01 +0100

    changes

---
+++ src/components/listAllPaintings.js
@@ -0,0 +1,33 @@
+import Flying from '../assets/flying.jpg';
+import Mountains from '../assets/mountains.jpg';
+import Cat from '../assets/cat.jpg';
+import Painting from '../components/painting';
+
+const paintings = [
+    {
+      title: 'Flying Carpet (1880)',
+      subtitle: 'Viktor Vasnetsov',
+      image:  Flying,
+      altTag: 'Flying Carpet by Victor Vasnetsov'
+    },
+    {
+      title: 'King Lake, California (1870-1875)',
+      subtitle: 'Albert Bierstadt',
+      image:  Mountains,
+      altTag: 'King Lake, California by Albert Bierstadt'
+    },
+    {
+      title: "Cat on a Cushion (L'hiver: Chat sur un coussin)  (1913)",
+      subtitle: "Théophile Alexandre Steinlen",
+      image: Cat,
+      altTag: "Winter: Cat on a Cushion by Théophile Alexandre Steinlen"
+    }
+  
+  ];
+  
+  export function ListAllPaintings() {
+    const paintingCards = paintings.map(painting =>
+      <Painting details={{ ...painting }}></Painting>
+    );
+    return <div className='paintings'>{paintingCards}</div>;
+  }
---
+++ src/components/painting.js
@@ -0,0 +1,17 @@
+import { IonImg, IonCard, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCardContent } from '@ionic/react';
+
+export default function Painting({details}) {
+
+    return (
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>{details.title}</IonCardTitle>
+            <IonCardSubtitle>{details.subtitle}</IonCardSubtitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            <IonImg src={details.image} alt={details.altTag}></IonImg>
+          </IonCardContent>
+        </IonCard>
+    )
+}
